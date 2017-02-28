69d9aa6eb0b20bac7d00bcafd4f01a4b908c901e
eight <eight04@gmail.com>
2017-02-28 11:42:36 +0800

    Compat with py2, add EncodePrinter

--- safeprint/__init__.py
+++ safeprint/__init__.py
@@ -2,91 +2,127 @@
 
 """A printer suppressing UnicodeEncodeError."""
 
+from __future__ import print_function
+
+import os
+import re
+import sys
+
 __version__ = "0.1.4"
 
-import os, re, sys, builtins
+PRINT = print
+TTY = sys.stdout.isatty()
+PY2 = sys.version_info < (3, 0, 0)
+PY36 = sys.version_info >= (3, 6, 0)
+WIN32 = sys.platform == "win32"
+
+try:
+	STR = unicode
+except NameError:
+	STR = str
 
 try:
 	import win_unicode_console.streams
 	WIN_UNICODE_CONSOLE = True
 except ImportError:
 	WIN_UNICODE_CONSOLE = False
-	
+
 def wuc_should_be_fixed():
 	stdout = getattr(win_unicode_console.streams, "STDOUT", None)
 	return not stdout or stdout.should_be_fixed()
 
-def Printer():
-	if sys.version_info >= (3, 6, 0):
-		return BasePrinter()
-		
-	if sys.platform != "win32":
-		return BasePrinter()
-		
-	if not WIN_UNICODE_CONSOLE:
-		return EchoPrinter
-		
-	if wuc_should_be_fixed():
-		return WinUnicodePrinter()
-		
-	return BasePrinter()
+def Printer(): # pylint: disable=invalid-name
+	if TTY:
+		if PY36 or not WIN32:
+			return BasePrinter()
+
+		if WIN_UNICODE_CONSOLE and wuc_should_be_fixed():
+			return WinUnicodePrinter()
+
+		if not PY2:
+			return EchoPrinter()
+
+		return TryPrinter()
+
+	if PY2:
+		return EncodePrinter()
+
+	return Py3EncodePrinter()
 
 class BasePrinter:
 	"""Create printer"""
 	def __init__(self):
 		self.listeners = set()
-		
-	def print(self, *objects, sep=" ", end="\n", file=sys.stdout, flush=False):
+
+	def print(self, *objects, **kwargs):
 		"""Micmic print interface"""
-		text = sep.join(map(str, objects))
-		
-		if file is not sys.stdout:
-			builtins.print(text, end=end, file=file, flush=flush)
+		file = kwargs.get("file")
+
+		if file is not None and file is not sys.stdout:
+			PRINT(*objects, **kwargs)
+
 		else:
-			self.imp_print(text, end=end)
-			
+			sep = STR(kwargs.get("sep", " "))
+			end = STR(kwargs.get("end", "\n"))
+
+			text = sep.join(STR(o) for o in objects)
+
+			self.imp_print(text, end)
+
 		for callback in self.listeners:
 			callback(text)
-			
+
 	def imp_print(self, text, end):
 		"""Implement"""
-		builtins.print(text, end=end)
-		
+		PRINT(text, end=end)
+
 	def add_listener(self, callback):
 		"""Register callback"""
 		self.listeners.add(callback)
-		
+
 	def remove_listener(self, callback):
 		"""Unregister callback"""
 		self.listeners.remove(callback)
-		
+
 class EchoPrinter(BasePrinter):
 	def imp_print(self, text, end):
 		"""Use windows echo"""
 		for line in text.split("\n"):
 			line = re.sub(r"[\&<>|^]", self.escape, line)
-			os.system('echo:' + line)
-			
+			command = ('echo:' + line).encode("UTF-16BE")
+			PRINT(str(0) in command)
+			os.system(command)
+
 	def escape(self, match):
 		"""Return escaped echo str."""
 		return "^" + match.group()
-		
+
 class TryPrinter(BasePrinter):
 	def imp_print(self, text, end):
 		"""Catch UnicodeEncodeError"""
 		try:
-			builtins.print(text, end=end)
+			PRINT(text, end=end)
 		except UnicodeEncodeError:
 			for i in text:
 				try:
-					builtins.print(i, end="")
+					PRINT(i, end="")
 				except UnicodeEncodeError:
-					builtins.print("?", end="")
-			builtins.print("", end=end)
-			
+					PRINT("?", end="")
+			PRINT("", end=end)
+
 class WinUnicodePrinter(BasePrinter):
 	def imp_print(self, text, end):
 		"""Use win_unicode_console"""
-		builtins.print(text, end=end, file=win_unicode_console.streams.stdout_text_transcoded)
-		
+		PRINT(text, end=end, file=win_unicode_console.streams.stdout_text_transcoded)
+
+class EncodePrinter(BasePrinter):
+	def imp_print(self, text, end):
+		"""Directly send utf8 str to stdout (py2)"""
+		sys.stdout.write((text + end).encode("utf-8"))
+
+class Py3EncodePrinter(BasePrinter):
+	def imp_print(self, text, end):
+		"""Directly send utf8 str to stdout (py2)"""
+		sys.stdout.buffer.write((text + end).encode("utf-8"))
+
 print = Printer().print
